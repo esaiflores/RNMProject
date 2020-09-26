4a04a06fda1db78ad9b4dff5715a97ec51ee017a
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-25 19:08:45 -0500

    creating table for database and using id, username, password, first and last name

---
+++ src/main/java/com/rnm/omdb/models/User.java
@@ -0,0 +1,13 @@
+package com.rnm.omdb.models;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255, unique = true)
+    private String username;
+}
