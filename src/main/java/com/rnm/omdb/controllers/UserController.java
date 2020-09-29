2b6235cf9659233c5c5e1139067f58d1e1636e92
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-29 00:30:02 -0500

    finished user controller

--- src/main/java/com/rnm/omdb/controllers/UserController.java
+++ src/main/java/com/rnm/omdb/controllers/UserController.java
@@ -1,7 +1,48 @@
 package com.rnm.omdb.controllers;
 
+import com.rnm.omdb.daos.UsersRepository;
+import com.rnm.omdb.models.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+    private UsersRepository usersDao;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UsersRepository usersDao, PasswordEncoder passwordEncoder) {
+        this.usersDao = usersDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User newUser, @Validated User user, Errors validation, Model model){
+        String username = user.getUsername();
+        User userExists = usersDao.findByUsername(username);
+
+        if (userExists != null) {
+            validation.rejectValue("username", "user.username", username + " already exists in our records.");
+        }
+        if (validation.hasErrors()) {
+            model.addAttribute("errors", validation);
+            model.addAttribute("user", user);
+            return "/sign-up";
+        }
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        usersDao.save(newUser);
+        return "redirect:/";
+    }
 }
