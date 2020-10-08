fb34c10758aed084bc0d250ecfdd23b000fe4062
Esai Flores <flores.esaijonathan@gmail.com>
2020-10-08 11:52:37 -0500

    feat: fixing bookmark table model

--- src/main/java/com/rnm/omdb/models/Bookmark.java
+++ src/main/java/com/rnm/omdb/models/Bookmark.java
@@ -1,45 +1,96 @@
 package com.rnm.omdb.models;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="bookmarks")
 public class Bookmark {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-    private String omdbId;
+    @Column(columnDefinition = "TEXT")
+    private String title;
 
-    private Integer ownerId;
+    @Column(columnDefinition = "TEXT")
+    private String year;
 
-    public Integer getId() {
-        return this.id;
+    @Column(columnDefinition = "TEXT")
+    private String posterLink;
+
+    @Column(columnDefinition = "TEXT")
+    private String imdbID;
+
+
+    public Bookmark() {
     }
 
-    public void setId(Integer id) {
+    public Bookmark(String title) {
+        this.title = title;
+    }
+
+    public Bookmark(String title, String year, String imdbID) {
+        this.title = title;
+        this.year = year;
+        this.imdbID = imdbID;
+    }
+
+    public Bookmark(String title, String year, String posterLink, String imdbID) {
+        this.title = title;
+        this.year = year;
+        this.posterLink = posterLink;
+        this.imdbID = imdbID;
+    }
+
+    public Bookmark(long id, String title, String year, String posterLink, String imdbID) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.posterLink = posterLink;
+        this.imdbID = imdbID;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
         this.id = id;
     }
 
-    public String getOmdbId() {
-        return this.omdbId;
+    public String getTitle() {
+        return title;
     }
 
-    public void setOmdbId(String omdbId) {
-        this.omdbId = omdbId;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public Integer getOwnerId() {
-        return this.ownerId;
+    public String getYear() {
+        return year;
     }
 
-    public void setOwnerId(Integer ownerId) {
-        this.ownerId = ownerId;
+    public void setYear(String year) {
+        this.year = year;
     }
 
+    public String getPosterLink() {
+        return posterLink;
+    }
+
+    public void setPosterLink(String posterLink) {
+        this.posterLink = posterLink;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
     
 
 
